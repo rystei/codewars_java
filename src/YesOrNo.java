2f8cce6b0cac914a5a03ca888acc03ef56e3740d
rystei <gustavo.telles@edu.unifil.br>
2025-05-14 11:00:28 -0300

    //Convert boolean values to strings 'Yes' or 'No'.

--- src/YesOrNo.java
+++ src/YesOrNo.java
@@ -1,3 +1,4 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 //8 kyu
 //Convert boolean values to strings 'Yes' or 'No'.
 class YesOrNo
